dd22167f9ed6b30b3e52f749de327fa87b6521d6
wutianwen <wutianwenzy@foxmail.com>
2023-10-28 18:17:29 +0800

    add file

---
+++ Spider/No001/tes001t.py
@@ -0,0 +1,73 @@
+"""
+10行代码集2000张美女图，Python爬虫120例，再上征途
+https://dream.blog.csdn.net/article/details/117024328
+"""
+import requests
+import re
+import time
+
+
+# 请求函数
+def request_get(url, ret_type="text", timeout=5, encoding="GBK"):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                      "Version/17.1 Safari/605.1.15"
+    }
+    res = requests.get(url=url, headers=headers, timeout=timeout)
+    res.encoding = encoding
+    if ret_type == "text":
+        return res.text
+    elif ret_type == "image":
+        return res.content
+
+
+# 抓取函数
+def main():
+    urls = [f"http://www.netbian.com/mei/index_{i}.htm" for i in range(2, 201)]
+    url = "http://www.netbian.com/qiche/index.htm"
+    urls.insert(0, url)
+    for url in urls:
+        print("抓取列表页地址为：", url)
+        text = request_get(url)
+        format(text)
+
+
+# 解析函数
+def analysis(text):
+    origin_text = split_str(text, '<div class="list">', '<div class="page">')
+    pattern = re.compile('href="(.*?)"')
+    hrefs = pattern.findall(origin_text)
+    hrefs = [i for i in hrefs if i.find("desk") > 0]
+    for href in hrefs:
+        url = f"http://www.netbian.com{href}"
+        print(f"正在下载：{url}")
+        text = request_get(url)
+        format_detail(text)
+
+
+def split_str(text, s_html, e_html):
+    start = text.find(s_html) + len(e_html)
+    end = text.find(e_html)
+    origin_text = text[start:end]
+
+    return origin_text
+
+
+def format_detail(text):
+    origin_text = split_str(text, '<div class="pic">', '<div class="pic-down">')
+    pattern = re.compile('src="(.*?)"')
+    image_src = pattern.search(origin_text).group(1)
+    # 保存图片
+    save_image(image_src)
+
+
+# 存储函数
+def save_image(image_src):
+    content = request_get(image_src, "image")
+    with open(f"./No001/{str(time.time())}.jpg", "wb") as f:
+        f.write(content)
+        print("图片保存成功")
+
+
+if __name__ == '__main__':
+    main()
---
+++ Spider/No002/test002.py
@@ -0,0 +1,71 @@
+"""
+通过Python爬虫，发现60%女装大佬游走在cosplay领域
+https://dream.blog.csdn.net/article/details/117221667
+"""
+import requests
+import time
+
+global total
+
+
+# 请求函数
+def request_get(url, ret_type="text", timeout=5, encoding="utf-8", host="bbs-api.mihoyo.com"):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+                      "Version/17.1 Safari/605.1.15",
+        "Origin": "https://bbs.mihoyo.com",
+        "Referer": "https://bbs.mihoyo.com/",
+        "Host": host
+    }
+    res = requests.get(url=url, headers=headers, timeout=timeout)
+    res.encoding = encoding
+    if ret_type == "text":
+        return res.text
+
+    elif ret_type == "image":
+        return res.content
+
+    elif ret_type == "json":
+        return res.json()
+
+
+# 保存图片
+def save_image(image_src):
+    content = request_get(image_src, "image", host="upload-bbs.mihoyo.com")
+    with open(f"./No002/{str(time.time())}.jpg", "wb") as f:
+        f.write(content)
+        global total
+        total += 1
+        print(f"保存第{total}张图片")
+
+
+# 抓取内页数据
+def detail(post_id):
+    url = f"https://bbs-api.mihoyo.com/post/wapi/getPostFull?gids=5&post_id={post_id}&read=1"
+    res_json = request_get(url, ret_type="json", timeout=5)
+    if res_json["retcode"] == 0:
+        image_list = res_json["data"]["post"]["image_list"]
+        for img in image_list:
+            img_url = img["url"]
+            if (img_url.find("weigui")) < 0:
+                save_image(img_url)
+
+
+# 抓取函数
+def main(last_id):
+    # 起始页面
+    url = f"https://bbs-api.mihoyo.com/post/wapi/getForumPostList?forum_id=47&gids=5&is_good=false&last_id={last_id}&is_hot=false&page_size=20&sort_type=2"
+    res_json = request_get(url, ret_type="json", timeout=5)
+    if res_json["retcode"] == 0:
+        for item in res_json["data"]["list"]:
+            # 抓取内页数据
+            detail(item["post"]["post_id"])
+
+    if res_json["data"]["last_id"] != "":
+        return main(res_json["data"]["last_id"])
+
+
+if __name__ == '__main__':
+    global total
+    total = 0
+    main(6356513)
